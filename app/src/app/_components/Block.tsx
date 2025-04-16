be1236dcee664d56c92d0e05cc052bccb5858314
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-04-16 17:44:03 -0400

    fix landing page

--- app/src/app/_components/Block.tsx
+++ app/src/app/_components/Block.tsx
@@ -12,7 +12,7 @@ export default function Block({
   children: ReactNode;
 }) {
   return (
-    <div className="bg-card h-auto w-auto min-w-[375px] justify-items-center rounded-lg bg-[#FFFFFF] p-5 shadow-[inset_0px_4px_22.600000381469727px_12px_rgba(0,0,0,0.89)] md:min-h-[500px] md:w-[25vw]">
+    <div className="bg-card h-auto w-auto min-w-[375px] items-center justify-center justify-items-center rounded-lg bg-[#FFFFFF] p-5 shadow-[inset_0px_4px_22.600000381469727px_12px_rgba(0,0,0,0.89)] md:min-h-[500px] md:w-[25vw]">
       <h1 className="font-['Instrument Sans'] pt-9 text-center text-4xl font-bold text-[#F2B417]">
         STEP {step}
       </h1>
