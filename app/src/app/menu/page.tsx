061fed5fbe4a81230e28fc8beb3a9467b880212b
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-05-02 01:00:11 -0400

    Top padding to the menu

--- app/src/app/menu/page.tsx
+++ app/src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import LoadSVG from "../_components/imports/load-svg";
 
 export default function Menu() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-6 bg-[radial-gradient(ellipse_156.03%_212.89%_at_93.68%_-5.52%,_#766251_0%,_#262013_35%,_#030303_77%,_#2F2817_100%)] p-4">
+    <div className="mt-14 flex h-screen flex-col items-center justify-center space-y-6 bg-[radial-gradient(ellipse_156.03%_212.89%_at_93.68%_-5.52%,_#766251_0%,_#262013_35%,_#030303_77%,_#2F2817_100%)] p-4 md:mt-0">
       <h1 className="font-['Instrument Sans'] text-center text-3xl leading-[40px] font-bold text-[#FFFFFF] not-italic md:text-5xl md:leading-[59px]">
         Upload Your File
       </h1>
