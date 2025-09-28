7f4c371d4f6cd6e9cc8379e7481d059974b5c994
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-09-28 19:04:30 -0400

    Updated eslint config file

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,48 +1,46 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import tseslint from "typescript-eslint";
+// eslint.config.js
+import next from "@next/eslint-plugin-next";
+import eslint from "eslint";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+const reactRules = {
+  'react/display-name': 'off',
+  'react/jsx-key': 'off',
+  'react/prop-types': 'off'
+};
 
-export default tseslint.config(
+/** @type {import('eslint').Linter.ConfigSchema['rules']} */
+const nextRules = {
+  '@next/next/no-html-link-for-pages': 'off'
+};
+
+const commonRules = {
+  'no-unused-vars': 'warn',
+  'no-console': 'warn',
+  'no-shadow': 'off',
+  'no-undef': 'warn'
+};
+
+export default [
   {
-    ignores: [".next"],
-  },
-  ...compat.extends("next/core-web-vitals"),
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    extends: [
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...tseslint.configs.stylisticTypeChecked,
-    ],
-    rules: {
-      "@typescript-eslint/array-type": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        { prefer: "type-imports", fixStyle: "inline-type-imports" },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        { checksVoidReturn: { attributes: false } },
-      ],
-    },
-  },
-  {
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       parserOptions: {
-        projectService: true,
-      },
+        project: true,
+        tsconfigRootDir: __dirname
+      }
     },
-  },
-);
+    plugins: {
+      '@next/next': next
+    },
+    rules: {
+      ...commonRules,
+      ...nextRules,
+      ...reactRules
+    },
+    settings: {
+      next: {
+        rootDir: __dirname
+      }
+    }
+  }
+];
